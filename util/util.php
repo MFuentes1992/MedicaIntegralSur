257297dfe7b9acb40649ae24a7c347cb137dfff1
MFuentes1992 <markfuentes2991@gmail.com>
2020-03-07 11:03:32 -0600

    Finall commit

--- util/util.php
+++ util/util.php
@@ -1,5 +1,4 @@
 <?php 
-
     //$url = "http://localhost/MedicaIntegralSur/";
     //$url = "http://localhost/Proyectos/Web/2020/EspecialidadesMedicas/";
     $url = " https://medicaintegralsur.com/";
